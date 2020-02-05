c2d7a55cc9ed150d71b3501e3a95fb293f83e9ba
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-02-05 15:02:05 +0900

    ログインフォーム作成

---
+++ src/main/java/jp/co/sample/form/LoginForm.java
@@ -0,0 +1,17 @@
+package jp.co.sample.form;
+
+/**
+ * ログイン時に使用するフォーム
+ * 
+ * @author yuuki
+ *
+ */
+public class LoginForm {
+	
+	/** メールアドレス */
+	private String mailAddress;
+	
+	/** パスワード */
+	private String password; 
+	
+}
