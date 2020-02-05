3927f8d558c4fd83b51db59ee33db0c33dbada6e
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-02-05 12:00:48 +0900

    EmployeeRepository method

--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -1,8 +1,69 @@
 package jp.co.sample.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
 
 @Repository
 public class EmployeeRepository {
 
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER 
+		= (rs, i)->{
+			Employee employee = new Employee();
+			employee.setId(rs.getInt("id"));
+			employee.setName(rs.getString("name"));
+			employee.setImage(rs.getString("image"));
+			employee.setGender(rs.getString("gender"));
+			employee.setHireDate(rs.getDate("hire_date"));
+			employee.setZipCode(rs.getString("zip_code"));
+			employee.setAddress(rs.getString("address"));
+			employee.setTelephone(rs.getString("telephone"));
+			employee.setSalary(rs.getInt("salary"));
+			employee.setCharacteristics(rs.getString("characteristics"));
+			employee.setDependentsCount(rs.getInt("dependents_count"));
+			return employee;
+		};
+		
+	public List<Employee> findAll(){		
+		String sql = "SELECT id, name, image, gender, hire_date, zip_code," + 
+					 "address, telephone, salary, characteristics, dependents_count " +
+					 "FROM employees ORDER BY hire_date";
+		
+		List <Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+		
+		return employeeList;
+	}
+	
+	
+	public Employee load(Integer id) {
+		String sql = "SELECT id, name, image, gender, hire_date, zip_code," + 
+				     "address, telephone, salary, characteristics, dependents_count " +
+				 	 "FROM employees WHERE id=:id";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		
+		Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+		
+		return employee;
+	}
+	
+	public void update(Employee employee) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		
+		String sql = "UPDATE employees SET dependents_count=:dependentsCount " +
+					 "WHERE id=:id ";
+		
+		template.update(sql, param);
+	}
 }
