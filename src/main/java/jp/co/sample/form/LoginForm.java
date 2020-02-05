fb1bccfe5b96ec8e112e73770a83551ff1943c67
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-02-05 15:24:12 +0900

    ログイン画面表示

--- src/main/java/jp/co/sample/form/LoginForm.java
+++ src/main/java/jp/co/sample/form/LoginForm.java
@@ -12,6 +12,30 @@ public class LoginForm {
 	private String mailAddress;
 	
 	/** パスワード */
-	private String password; 
+	private String password;
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + ", getMailAddress()="
+				+ getMailAddress() + ", getPassword()=" + getPassword() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	} 
+	
 	
 }
